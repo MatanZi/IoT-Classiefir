04f0f6604fbbb437031cc00c73e6dfbf96747582
Idan <user@LAPTOP-4LI6OEPQ>
2019-09-13 04:37:30 +0300

    Update SVM machine learning

--- __main__.py
+++ __main__.py
@@ -1,5 +1,6 @@
 from csv_handler.csv_builder import *
 from machine_learning_algorithms.random_forest import RF
+from machine_learning_algorithms.SVM import SVM
 
 
 
@@ -8,17 +9,20 @@ def main():
     mac_list = ["98:fc:11:a1:f7:0f", "6c:fd:b9:4f:70:0b", "00:17:88:77:35:80", "fc:6b:f0:0a:c3:43"]
     N = 5
 
-    dataset_file =  r"C:\Users\Dan\PycharmProjects\IoT-Classiefir\outfile0505.csv"
-    features_csv_path = r"C:\Users\Dan\PycharmProjects\IoT-Classiefir\features1.csv"
+    dataset_file =  r"C:\Users\user\Desktop\final_project_new\outfile0505.csv"
+    features_csv_path = r"C:\Users\user\PycharmProjects\IoT-Classiefir\features1.csv"
     build_features_csv(dataset_file, ip_list, N, features_csv_path)
 
     features_df = pd.read_csv(features_csv_path)
-    factor = pd.factorize(features_df.iloc[:, 4])
-    X = features_df.iloc[:, :4]
-    y = features_df.iloc[:, 4]
-    definitions = factor[1]
+    X = features_df.drop('label', axis=1)
+    y = features_df['label']
+    SVM(X,y)
+    #factor = pd.factorize(features_df.iloc[:, 4])
+    #X = features_df.iloc[:, :4]
+    #y = features_df.iloc[:, 4]
+    #definitions = factor[1]
 
-    RF(X, y, definitions)
+    #RF(X, y, definitions)
 
 
 if __name__ == '__main__':
--- machine_learning_algorithms/SVM.py
+++ machine_learning_algorithms/SVM.py
@@ -15,21 +15,12 @@ import pandas as pd
 #y = packdata['Class']
 
 def SVM(X,y):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
 
-    clf = SVC(gamma='scale', decision_function_shape='ovo')
-    clf.fit(X,y)
-    SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
-        decision_function_shape='ovo',degree=3,gamma='scale', kernel='rbf',
-        max_iter=-1, probability=False, random_state=None, shrinking=True,
-        tol=0.001, verbose=False)
-    dec = clf.decision_function([[1]])
-    print(dec.shape[1])
+    svclassifier = SVC(kernel='rbf')
+    svclassifier.fit(X_train, y_train)
 
-    #svm_predictions = svm_model_linear.predict(X_test)
+    y_pred = svclassifier.predict(X_test)
 
-    #accuracy = svm_model_linear.score(X_test, y_test)
-
-    #cm = confusion_matrix(y_test, svm_predictions)
-    #print(confusion_matrix(y_test, y_pred))
-    #print(classification_report(y_test,y_pred))
+    print(confusion_matrix(y_test, y_pred))
+    print(classification_report(y_test,y_pred))
